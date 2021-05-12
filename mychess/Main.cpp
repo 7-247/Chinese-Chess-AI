5c1a7c490e60dc328c77529f551926ea261e0ba2
youarenot2b <1262454489@qq.com>
2021-05-12 14:51:17 +0800

    my

--- mychess/Main.cpp
+++ mychess/Main.cpp
@@ -1,14 +1,14 @@
 #include "ChessBoard.h"
 
-
 inline void PrintLn(const char* sz) {
-	printf("%s\n", sz);
-	fflush(stdout);
+   printf("%s\n", sz);
+   fflush(stdout);
 }
 int main() {
-	char arr[600]="rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1";
-	PositionStruct mychess(arr);
-	ChessBoard myboard(mychess);
-	myboard.DrawBoard();
-	return 0;
+   char arr[600] =
+       "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1";
+   PositionStruct mychess(arr);
+   ChessBoard myboard(mychess);
+   myboard.DrawBoard();
+   return 0;
 }
