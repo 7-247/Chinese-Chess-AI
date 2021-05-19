77b3e5ee57b93f1601d38d22790e870fe2990caa
caoyandidi1103 <didiking@yeah.net>
2021-05-19 11:13:57 +0800

    com

--- C++_project_build.bat
+++
@@ -1,4 +0,0 @@
-setlocal enabledelayedexpansion
-for %%i in (*.cpp) do set var1=!var1! %%i
-echo !var1! 
-g++ !var1! -o D:\main -Wall -O2 -std=c++11 && start cmd /c "chcp 65001 & cls & main & echo. & pause"
