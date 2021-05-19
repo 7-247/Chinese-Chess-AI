1ff2fd974c150cf6197c3f320b2cfb19e6e5a330
caoyandidi1103 <didiking@yeah.net>
2021-05-19 11:24:28 +0800

    togbk

--- C++_project_build.bat
+++ C++_project_build.bat
@@ -1,4 +1,4 @@
 setlocal enabledelayedexpansion
 for %%i in (*.cpp) do set var1=!var1! %%i
 echo !var1! 
-g++ !var1! -o main -Wall -O2 -std=c++11 && start cmd /c "chcp 65001 & cls & main & echo. & pause"
+g++ !var1! -o main -Wall -O2 -std=c++11 && start cmd /c "chcp 936 & cls & main & echo. & pause"
