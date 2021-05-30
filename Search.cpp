701564a86ddfa89383d895625faa78dc374c4bc9
caoyandidi1103 <didiking@yeah.net>
2021-05-30 22:46:03 +0800

    根据机能判定搜索深度；删除注释

--- Search.cpp
+++ Search.cpp
@@ -10,17 +10,14 @@
 clock_t startTime = 0;
 const int MaxDepth = 20;
 Eval myeval;
-bool tiaoshismall = 1;
-bool tiaoshi = 0;
-bool tiaoshibig = 0;
 int DEPTH = 5;
 int gloTime;
+int gloDep = 5;
 vector<int> nodeNum[MaxDepth + 5];
 int totalNode;
 struct tree {
     PositionStruct chess;
     int childnode[10];
-    // bool childuse[5];  // 1在用 0抛弃
     int childmove[10];
     int fathernode, eval, nowbestnode;
     bool inuse;
@@ -30,19 +27,18 @@ struct allmoves {
     PositionStruct chess;
     int eval, move;
     bool operator<(const allmoves& x) { return this->chess.sdPlayer == 0 ? (this->eval < x.eval) : (this->eval > x.eval); }
-    // allmoves(PositionStruct chess, int eval, int move) : chess(chess), eval(eval), move(move){};
 };
 
 vector<allmoves> am;
 
-int bestmove;  // 记录返回的步数
+int bestmove;
 int lastbestmove = 0;
 static PositionStruct turn;
 static const int turnArr[100] = {0,  9,  8,  7,  6,  5,  4,  3,  2,  1,  18, 17, 16, 15, 14, 13, 12, 11, 10, 27, 26, 25, 24, 23, 22, 21, 20, 19, 36, 35, 34,
                                  33, 32, 31, 30, 29, 28, 45, 44, 43, 42, 41, 40, 39, 38, 37, 54, 53, 52, 51, 50, 49, 48, 47, 46, 63, 62, 61, 60, 59, 58, 57,
                                  56, 55, 72, 71, 70, 69, 68, 67, 66, 65, 64, 81, 80, 79, 78, 77, 76, 75, 74, 73, 90, 89, 88, 87, 86, 85, 84, 83, 82};
 static int chessType[40] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 13, 13, 13};
-//帅仕仕相相马马车车炮炮兵兵兵兵兵(将士士象象马马车车炮炮卒卒卒卒卒)
+
 static map<long long, int> subtable[MaxDepth];
 long long chessHash(PositionStruct& mychess) {
     long long hs = 0;
@@ -59,12 +55,10 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
     if (clock() - startTime >= gloTime) return 100000005;
     int state = mychess.Repeat();
 
-    if (mychess.nowPos[0] == 0 || state == 1) return -6666666;  //红死 红长将
-    if (mychess.nowPos[16] == 0 || state == 2) return 6666666;  //黑死 黑长将
+    if (mychess.nowPos[0] == 0 || state == 1) return -6666666;
+    if (mychess.nowPos[16] == 0 || state == 2) return 6666666;
 
     if (state == 0 || depth + extend <= 0) {
-        // ChessBoard outp(mychess);
-        // outp.DrawBoard();
         myeval.EvalInit(mychess);
         int tepvalue = myeval.GetEvalNum();
         if (state == 0) {
@@ -72,15 +66,9 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
             cout << tepvalue << endl;
             if ((mychess.sdPlayer == 1 && tepvalue < -300) || (mychess.sdPlayer == 0 && tepvalue > 300)) {
                 cout << "拉回0\n";
-                //当前局面走棋方优势较大，但会导致和棋，所以将评估值拉向0
                 return 0;
             } else
-                //当前局面走棋方为劣势，且会导致和棋，所以希望和棋，所以走这一步的意愿加大。
                 return tepvalue - 1000 * mychess.sdPlayer + 500;
-        }
-        if (tiaoshibig) {
-            for (int i = DEPTH; i >= depth; --i) cout << "      ";
-            cout << tepvalue << endl;
         }
         return tepvalue;
     }
@@ -92,10 +80,9 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
     memset(chessBoard, -1, sizeof(chessBoard));
     for (int i = 0; i < 32; ++i) chessBoard[mychess.nowPos[i]] = i;
 
-    vector<vector<int>> moves = mychess.Generate();  //获取走法
+    vector<vector<int>> moves = mychess.Generate();
     int x = mychess.sdPlayer;
 
-    // 获取所有走法
     vector<heuristic> h;
     for (int i = x * 16; i < x * 16 + 16; ++i)
         for (auto nowPos : moves[i]) {
@@ -111,25 +98,6 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
             int ttv = hh[mychess.nowPos[i]][nowPos];
             if ((typ == 3 || typ == 4 || typ == 10 || typ == 11) && ttv == 0) ttv = 1;
             if (DEPTH == depth && ((mychess.nowPos[i] << 8) + nowPos == lastbestmove)) ttv += 100000000;
-            /*
-            if (mychess.sdPlayer == 1) {
-                if (mychess.sdPlayer) mychess.sdPlayer = !mychess.sdPlayer;
-                int pastPos = mychess.nowPos[i];
-                mychess.nowPos[i] = nowPos;
-                int isenemy = chessBoard[nowPos];
-                if (isenemy != -1) mychess.nowPos[isenemy] = 0;
-
-                myeval.EvalInit(mychess);
-                if (mychess.sdPlayer == 1)
-                    ttv += myeval.GetEvalNum();
-                else
-                    ttv -= myeval.GetEvalNum();
-
-                mychess.sdPlayer = !mychess.sdPlayer;
-                mychess.nowPos[i] = pastPos;
-                if (isenemy != -1) mychess.nowPos[isenemy] = nowPos;
-            }
-            */
             h.push_back({mychess.nowPos[i], nowPos, ttv});
         }
     vector<vector<int>>().swap(moves);
@@ -139,25 +107,14 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
     else {
         sort(h.begin(), h.end());
         int testCount = 1000;
-        /*
-        int testCount = min(int(h.size()), index[DEPTH - depth]);
-        if (testCount >= 16 && h[testCount / 2].hh)
-            while (h[testCount].hh == 0) testCount--;
-        */
-
         int nowbestmove = 0;
         bool isfirst = 1;
         bool bre = 0;
         for (auto ttem : h) {
-            if (tiaoshibig) {
-                for (int i = DEPTH; i >= depth; --i) cout << "      ";
-                cout << ttem.src << " " << ttem.des << " " << mychess.sdPlayer << endl;
-            }
-
             if (!testCount) break;
             testCount--;
             int pastPos = ttem.src;
-            if (pastPos == 0) continue;  //已被吃掉则不用管
+            if (pastPos == 0) continue;
             int i = chessBoard[pastPos];
             int nowPos = ttem.des;
             long long newHsh = nowHsh ^ hsh[chessType[i]][pastPos] ^ hsh[chessType[i]][nowPos];
@@ -169,51 +126,26 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
             if (isenemy != -1) {
                 newHsh ^= hsh[chessType[isenemy]][nowPos];
                 mychess.Count = 0;
-                mychess.nowPos[isenemy] = 0;  //找到了，要吃掉
+                mychess.nowPos[isenemy] = 0;
             } else
                 ++mychess.Count;
             int value;
             if (isfirst) {
                 isfirst = 0;
-                // value = alphabeta(mychess, depth - 1, alpha, beta, newHsh, isenemy == -1 ? 0 : 4);
                 value = alphabeta(mychess, depth - 1, alpha, beta, newHsh, isenemy == -1 ? 0 : 1);
-                if (tiaoshibig) {
-                    for (int i = DEPTH; i >= depth; --i) cout << "      ";
-                    cout << alpha << " " << value << " " << beta << endl;
-                }
                 if (depth == DEPTH) bestmove = (pastPos << 8) + nowPos;
                 nowbestmove = pastPos * 100 + nowPos;
             } else {
                 if (mychess.sdPlayer == 1) {
-                    // value = alphabeta(mychess, depth - 1, alpha, alpha + 1, newHsh, isenemy == -1 ? 0 : 4);
                     value = alphabeta(mychess, depth - 1, alpha, alpha + 1, newHsh, isenemy == -1 ? 0 : 1);
-                    if (tiaoshibig) {
-                        for (int i = DEPTH; i >= depth; --i) cout << "      ";
-                        cout << alpha << " " << value << " " << alpha + 1 << endl;
-                    }
                     if (alpha < value && value < beta) {
-                        // value = alphabeta(mychess, depth - 1, value, beta, newHsh, isenemy == -1 ? 0 : 4);
                         value = alphabeta(mychess, depth - 1, value, beta, newHsh, isenemy == -1 ? 0 : 1);
-                        if (tiaoshibig) {
-                            for (int i = DEPTH; i >= depth; --i) cout << "      ";
-                            cout << " / " << value << " " << beta << endl;
-                        }
                     }
 
                 } else {
-                    // value = alphabeta(mychess, depth - 1, beta - 1, beta, newHsh, isenemy == -1 ? 0 : 4);
                     value = alphabeta(mychess, depth - 1, beta - 1, beta, newHsh, isenemy == -1 ? 0 : 1);
-                    if (tiaoshibig) {
-                        for (int i = DEPTH; i >= depth; --i) cout << "      ";
-                        cout << beta - 1 << " " << value << " " << beta << endl;
-                    }
                     if (alpha < value && value < beta) {
-                        // value = alphabeta(mychess, depth - 1, alpha, value, newHsh, isenemy == -1 ? 0 : 4);
                         value = alphabeta(mychess, depth - 1, alpha, value, newHsh, isenemy == -1 ? 0 : 1);
-                        if (tiaoshibig) {
-                            for (int i = DEPTH; i >= depth; --i) cout << "      ";
-                            cout << alpha << " " << value << " / " << endl;
-                        }
                     }
                 }
             }
@@ -221,7 +153,7 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
                 bre = 1;
                 goto br;
             }
-            if (mychess.sdPlayer == 1) {  // Max 面向的是下一个结点
+            if (mychess.sdPlayer == 1) {
                 if (value > alpha) {
                     best = alpha = value;
                     if (depth == DEPTH) bestmove = (pastPos << 8) + nowPos;
@@ -238,7 +170,7 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
                 am.push_back({mychess, value, pastPos * 100 + nowPos});
             }
         br:
-            //复原
+
             mychess.sdPlayer = !mychess.sdPlayer;
             mychess.nowPos[i] = pastPos;
             mychess.Moves.pop_back();
@@ -251,14 +183,14 @@ int alphabeta(PositionStruct& mychess, int depth, int alpha, int beta, long long
             }
             if (alpha > beta) break;
         }
-        hh[nowbestmove / 100][nowbestmove % 100] += int(pow(2, depth));  // 历史启发
+        hh[nowbestmove / 100][nowbestmove % 100] += int(pow(2, depth));
     }
 
-    h.clear();  // 两种清空内存方法
+    h.clear();
     if (best != -123456789 && beta - alpha > 1) {
-        subtable[depth + 4][nowHsh] = best;  // 历史记录
+        subtable[depth + 4][nowHsh] = best;
         for (int i = 0; i < 32; ++i) turn.nowPos[i] = turnArr[mychess.nowPos[i]];
-        subtable[depth + 4][chessHash(turn)] = best;  // 记录翻转局面
+        subtable[depth + 4][chessHash(turn)] = best;
         return best;
     }
 
@@ -271,6 +203,13 @@ int oldSearchMain(PositionStruct& mychess, int nownode, int nowDepth) {
     for (int i = 0; i < MaxDepth; ++i) subtable[i].clear();
     int nowvalue = -100000000;
     nowvalue = alphabeta(mychess, DEPTH, -100000000, 100000000);
+    if (nownode == 1 && nowDepth == 1) {
+        if (clock() - startTime < 3000) {
+            DEPTH = 5;
+            gloDep = 6;
+            nowvalue = alphabeta(mychess, DEPTH, -100000000, 100000000);
+        }
+    }
     if (nowvalue == 100000005) return nowvalue;
     node[nownode].eval = nowvalue;
     sort(am.begin(), am.end());
@@ -281,7 +220,7 @@ int oldSearchMain(PositionStruct& mychess, int nownode, int nowDepth) {
         if (cnt >= 5 && (abs(node[totalNode].eval - i.eval) > 10 || nowDepth > 1)) break;
         ++totalNode;
         node[nownode].childnode[cnt] = totalNode;
-        // node[nownode].childuse[cnt] = 1;
+
         node[nownode].childmove[cnt] = i.move;
         nodeNum[nowDepth + 1].push_back(totalNode);
         node[totalNode].fathernode = nownode;
@@ -300,6 +239,7 @@ void discard(int x) {
         if (node[node[x].childnode[i]].inuse) discard(node[x].childnode[i]);
 }
 int SearchMain(PositionStruct& mychess, int gotime) {
+    gloDep = 5;
     startTime = clock();
     int lastTime = startTime;
     gloTime = max(gotime - 1000, gotime * 9 / 10);
@@ -310,31 +250,13 @@ int SearchMain(PositionStruct& mychess, int gotime) {
     node[1].chess = mychess;
     node[1].inuse = 1;
 
-    // for (int nowDepth = 1; nowDepth <= 1; nowDepth += 1) {
     for (int nowDepth = 1; nowDepth <= MaxDepth - 5; nowDepth += 1) {
-        DEPTH = max(5 - nowDepth, 3);
+        DEPTH = max(gloDep - nowDepth, 3);
         for (auto j : nodeNum[nowDepth])
             if (node[j].inuse) {
                 int nowDepthEval = oldSearchMain(node[j].chess, j, nowDepth);
                 if (nowDepthEval == 100000005) goto op;
                 node[j].eval = nowDepthEval;
-                if (tiaoshi) {
-                    for (int k = 1; k < nowDepth; ++k) cout << "   ";
-                    cout << j << "/" << totalNode << "  " << clock() - startTime << endl;
-                }
-
-                // ChessBoard outp(node[j].chess);
-                // outp.DrawBoard();
-                /*
-                for (int i = 0; i < 5; ++i) {
-                    if (node[node[j].childnode[i]].inuse) {
-                        for (int k = 1; k < nowDepth; ++k) cout << "   ";
-                        int tm = node[j].childmove[i];
-                        printf("%02d->%02d eval=", tm / 100, tm % 100);
-                        cout << node[node[j].childnode[i]].eval << endl;
-                    }
-                }
-                */
             }
         for (auto j : nodeNum[nowDepth - 1]) node[j].chess.Moves.clear();
         for (int i = nowDepth - 1; i; --i) {
@@ -363,25 +285,6 @@ int SearchMain(PositionStruct& mychess, int gotime) {
             for (auto j : nodeNum[i + 1])
                 if (abs(node[j].eval - node[node[j].fathernode].eval) > 50) discard(j);
         }
-        if (tiaoshi) {
-            for (int i = 1; i <= nowDepth; ++i) {
-                for (auto j : nodeNum[i])
-                    if (node[j].inuse) {
-                        for (int k = 1; k < i; ++k) cout << "      ";
-                        printf("%4d   %4d        ", node[j].eval, node[j].nowbestnode);
-                        for (int k = 0; k < 10; ++k)
-                            if (node[node[j].childnode[k]].inuse) printf("%4d ", node[node[j].childnode[k]].eval);
-
-                        cout << endl;
-                    }
-            }
-        }
-        if (tiaoshismall) {
-            printf("Depth=%-2d timeUse=%-5d totalTimeUse=%-5d\n", nowDepth, clock() - lastTime, clock() - startTime);
-            for (int i = 0; i < 10; ++i)
-                if (node[node[1].childnode[i]].inuse) printf("%04d  %d\n", node[1].childmove[i], node[node[1].childnode[i]].eval);
-        }
-
         if (clock() - lastTime > gotime / 2) goto op;
         lastTime = clock();
         if (abs(node[1].eval) > 6000000) {
